231cb4bc2c86762ac48912a735862f4ae78a8afb
Notkenftr <justkenftr@gmail.com>
2025-10-26 17:45:13 +0700

    r

--- plugins/example.disable/event.py
+++
@@ -1,8 +0,0 @@
-from Swit.SwitSDK import config
-
-async def on_start(app):
-    config.make(config_data='assets/config_data.yml')
-    print('Đã nhận on start')
-
-async def on_stop(app):
-    print('Đã nhận on stop')
